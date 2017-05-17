ecfe771b6ff205fc4b2b0ec6af287b130b8c2efe
Thiefgard <Thiefgard@gmail.com>
2017-05-17 20:33:33 +0300

    init commit

---
+++ collection/ContactEntry.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collection
+{
+    class ContactEntry
+    {
+        public ContactEntry()
+        {
+            LastName = "";
+            FirstName = "";
+            PhoneNumber = "";
+            Email = "";
+        }
+
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1}; Tel.: {2}; Email: {3}", LastName, FirstName, PhoneNumber, Email);
+        }
+    }
+}
